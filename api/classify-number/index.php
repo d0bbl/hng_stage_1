82e7519dcb30a500d3d3f52135ea1fd78119b92e
d0bbl <kennybajomo@gmail.com>
2025-02-09 09:11:00 +0100

    update index.php

--- api/classify-number/index.php
+++ api/classify-number/index.php
@@ -179,9 +179,9 @@ $response = [
         "prime_number" => isPrime($num),
         "perfect_number" => isPerfect($num),
         "digit_sum" => sumDigits($num_array),
-        "properties" => [ checkArmstrong($num, $sum_total),
+        "properties" => array_filter([ checkArmstrong($num, $sum_total),
           checkPolarity($num)
-        ],
+        ]),
         "digit_sum" => sumDigits($num_array),
         "fun_fact" => $response_array['text']
     ];
